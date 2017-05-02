d477d24ae6dc167a8e3d53a5e039501aa5be2f23
Alejandro Delgado Gandia <alejandro.gandia9@gmail.com>
2017-05-02 13:14:19 +0200

    refactor...

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,12 @@
 angular.module('queryExample')
 	.controller('queryController', function($scope, $http) {
+		var API_KEY = "be78ab7bb822bf90173073ba3336431b403e1ce5"
 		$scope.getGames = function(gameTitle) {
 			console.log("HEY")
 			console.log("From HTML we recived => " + gameTitle)
+			
 			// this is the FIRST endpoint api!
-			return $http.post('https://floating-mountain-70543.herokuapp.com/getInfo', { "gameTitle": gameTitle })
+			return $http.get("https://floating-mountain-70543.herokuapp.com/search/?api_key=" + API_KEY + "&format=json&query=" + gameTitle + "&resources=game")
 				 //PROMISE THEN here...
 				.then(function(response) {
 					$scope.gamesInfo = response.data.results
@@ -19,7 +21,7 @@ angular.module('queryExample')
 		$scope.getInfo = function(gameId) {
 			console.log(gameId)
 			// this is the SECOND endpoint api!
-			return $http.post('https://floating-mountain-70543.herokuapp.com/getMoreInfo', { "gameId": gameId })
+			return $http.get("https://floating-mountain-70543.herokuapp.com/search/ " + gameId +"?api_key=" + API_KEY)
 				//promise HERE
 				.then(function(response) {
 					$scope.gamesDetails = response.data.results
